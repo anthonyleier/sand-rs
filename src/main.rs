3fe4166446fd04aa070e54724d7d848663d9579a
Anthony Cruz <anthonyleierlw@gmail.com>
2024-02-08 16:48:37 -0300

    Lógica de matrizes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+fn montar_grade(linhas: usize, colunas: usize) -> Vec<Vec<i32>> {
+    let mut grade: Vec<Vec<i32>> = Vec::new();
+
+    for _ in 0..linhas {
+        let linha: Vec<i32> = vec![0; colunas];
+        grade.push(linha);
+    }
+
+    return grade;
+}
+
+fn imprimir_grade(grade: Vec<Vec<i32>>) {
+    for i in 0..grade.len() {
+        for j in 0..grade.len() {
+            print!("[{}]", grade[i][j]);
+        }
+        println!("");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut grade = montar_grade(5, 5);
+    grade[3][4] = 1;
+    imprimir_grade(grade);
 }
