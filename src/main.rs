c0b1ed496029da78ac7adb585ee182c4e64e7d98
Anthony Cruz <anthonyleierlw@gmail.com>
2024-02-09 11:06:36 -0300

    Melhorias na matriz

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use ggez::{
     graphics::Text,
     *,
 };
+mod matriz;
+use matriz::Matriz;
 
 struct State {
     dt: std::time::Duration,
@@ -25,25 +27,30 @@ impl ggez::event::EventHandler<GameError> for State {
     }
 }
 
-pub fn main() {
-    let state = State {
-        dt: std::time::Duration::new(0, 0),
-    };
+// pub fn main() {
+//     let state = State {
+//         dt: std::time::Duration::new(0, 0),
+//     };
 
-    let mut c = conf::Conf::new();
-    c.window_mode = WindowMode {
-        width: 200.0,
-        height: 200.0,
-        ..Default::default()
-    };
-    c.window_setup = WindowSetup {
-        title: "Sand".to_owned(),
-        ..Default::default()
-    };
+//     let mut c = conf::Conf::new();
+//     c.window_mode = WindowMode {
+//         width: 200.0,
+//         height: 200.0,
+//         ..Default::default()
+//     };
+//     c.window_setup = WindowSetup {
+//         title: "Sand".to_owned(),
+//         ..Default::default()
+//     };
 
-    let (ctx, event_loop) = ContextBuilder::new("Sand", "anthonyleier")
-        .default_conf(c)
-        .build()
-        .unwrap();
-    event::run(ctx, event_loop, state);
+//     let (ctx, event_loop) = ContextBuilder::new("Sand", "anthonyleier")
+//         .default_conf(c)
+//         .build()
+//         .unwrap();
+//     event::run(ctx, event_loop, state);
+// }
+
+fn main() {
+    let matriz = Matriz::new(10, 10);
+    println!("{:?}", matriz);
 }
--- src/matriz.rs
+++ src/matriz.rs
@@ -1,13 +1,25 @@
+use core::fmt;
+
 pub struct Matriz {
-    data: Vec<Vec<u8>>,
+    grade: Vec<Vec<u8>>,
 }
 
 impl Matriz {
     pub fn new(linhas: usize, colunas: usize) -> Matriz {
         Matriz {
-            data: vec![vec![0; colunas]; linhas],
+            grade: vec![vec![0; colunas]; linhas],
         }
     }
+}
 
-    // ... outras funções para acessar e modificar a matriz
+impl fmt::Debug for Matriz {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for linha in &self.grade {
+            for &valor in linha {
+                write!(f, "[{}]", valor)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
